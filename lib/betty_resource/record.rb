1562ade945bfc0467777e8445172543fc427e74b
Daniel Willemse <daniel.willemse@holder.nl>
2012-03-16 14:31:07 +0100

    Move definition of set method to new method

--- lib/betty_resource/record.rb
+++ lib/betty_resource/record.rb
@@ -5,15 +5,21 @@ module BettyResource
     def initialize(args)
       @model = args[:model]
       model.properties.each do |property|
-        define_singleton_method("#{property.name}=") do |val|
-          attributes[property.name] = val
-        end
+        define_setter(property)
       end
     end
 
     def attributes
       @attributes ||= model.properties.inject(HashWithIndifferentAccess.new()) do |hash, property|
         hash.merge(property.name => nil)
+      end
+    end
+
+    private
+
+    def define_setter(property)
+      define_singleton_method("#{property.name}=") do |val|
+        attributes[property.name] = val
       end
     end
   end
