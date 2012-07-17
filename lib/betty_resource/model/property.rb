52aaefcf4fef5ac312e9759ffffa550aecec2452
Paul Engel <paul.engel@holder.nl>
2012-07-18 00:52:55 +0200

    Trivial change

--- lib/betty_resource/model/property.rb
+++ lib/betty_resource/model/property.rb
@@ -3,14 +3,14 @@ module BettyResource
     class Property
       attr_accessor :id, :name, :kind
 
-      def initialize(id, name, kind)
-        @id, @name, @kind = id, name, kind
-      end
-
       def self.parse(input)
         input.collect do |row|
           Property.new(row["id"], row["name"], row["kind"])
         end
+      end
+
+      def initialize(id, name, kind)
+        @id, @name, @kind = id, name, kind
       end
 
     end
