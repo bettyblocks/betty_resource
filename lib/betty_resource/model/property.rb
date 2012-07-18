047750258da271a751cc3ba333c778540c708fad
Paul Engel <paul.engel@holder.nl>
2012-07-18 13:38:12 +0200

    Provided BettyResource::Model::Property.options

--- lib/betty_resource/model/property.rb
+++ lib/betty_resource/model/property.rb
@@ -1,16 +1,16 @@
 module BettyResource
   class Model < Base
     class Property
-      attr_accessor :id, :name, :kind
+      attr_accessor :id, :name, :kind, :options
 
       def self.parse(input)
         input.collect do |row|
-          Property.new(row["id"], row["name"], row["kind"])
+          Property.new(row["id"], row["name"], row["kind"], row["options"])
         end
       end
 
-      def initialize(id, name, kind)
-        @id, @name, @kind = id, name, kind
+      def initialize(id, name, kind, options)
+        @id, @name, @kind, @options = id, name, kind, options
       end
 
     end
