d6981091f41c51aa71f13814c75cdd177e48934c
Paul Engel <paul.engel@holder.nl>
2012-07-18 13:36:11 +0200

    Getting models metadata at initial request

--- lib/betty_resource/meta_data.rb
+++ lib/betty_resource/meta_data.rb
@@ -1,12 +1,12 @@
 module BettyResource
   class MetaData < Base
 
-    def initialize
-      @models = BettyResource::Model.parse(self.class.get("/models").parsed_response)
+    def models
+      @models ||= BettyResource::Model.parse(self.class.get("/models").parsed_response)
     end
 
     def model(name)
-      @models[name.to_s]
+      models[name.to_s]
     end
 
   end
