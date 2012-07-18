320baaed48c8866dcba9a507192959f502563f33
Paul Engel <paul.engel@holder.nl>
2012-07-18 09:19:59 +0200

    Fixed error before running tests (weird because I could run all tests without problems at home)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,10 @@ ENV["TESTOPTS"] = "-v"
 require "rubygems"
 require "bundler"
 
-Bundler.require :gem_default, :gem_test
-
 require "minitest/unit"
 require "minitest/autorun"
+
+Bundler.require :gem_default, :gem_test
 
 BettyResource.configure do |config|
   config.host     = "https://betty-resource-test.bettyblocks.com"
