d8040b48ff1653860ec767bdeb1dbfba672956bc
Paul Engel <paul.engel@holder.nl>
2013-02-07 00:48:27 +0100

    Adjusted Gemfile a bit Supporting credentials.yaml

--- Gemfile
+++ Gemfile
@@ -4,11 +4,15 @@ gemspec
 
 group :gem_default do
   gem "betty_resource", :path => "."
-  gem "colorize"
   gem "pry"
+end
+
+group :gem_development do
+  gem "colorize"
 end
 
 group :gem_test do
   gem "minitest"
   gem "mocha", :require => "mocha/setup"
+  gem "rake"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,12 @@ require "bundler"
 require "minitest/unit"
 require "minitest/autorun"
 
-
-
 Bundler.require :gem_default, :gem_test
-BettyResource.configure(MultiJson.decode(File.read(File.expand_path('../../.credentials', __FILE__))))
+
+if File.exists?(file = File.expand_path('../../.credentials', __FILE__))
+  BettyResource.configure MultiJson.decode(File.read(file))
+end
+
+if File.exists?(file = File.expand_path('../../credentials.yaml', __FILE__))
+  BettyResource.configure YAML.load_file(file)
+end
