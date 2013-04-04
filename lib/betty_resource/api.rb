c5b18652ffb30751b7d55f957c97ddc55fc515b4
Stephan Kaag <stephan@ka.ag>
2013-04-04 17:11:40 +0200

    Normalize query strings as json

--- lib/betty_resource/api.rb
+++ lib/betty_resource/api.rb
@@ -5,6 +5,8 @@ module BettyResource
     base_uri "#{BettyResource.config.host}/api"
     format :json
     basic_auth BettyResource.config.user, BettyResource.config.password
-
+    query_string_normalizer proc{|query|
+      query.to_json
+    }
   end
 end
